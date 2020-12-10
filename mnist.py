576f87ed852358616fbcd826976a22537952f272
Jonathan Lam <jlam55555@gmail.com>
2020-12-09 21:35:06 -0500

    added mnist training script

---
+++ mnist.py
@@ -0,0 +1,22 @@
+from models.mnist_classifier import MNISTClassifier
+
+mnist_classifier = MNISTClassifier()
+# mnist_classifier.build_fc_direct_model()
+# mnist_classifier.model.summary()
+# mnist_classifier.train(epochs=10)
+# mnist_classifier.evaluate()
+
+# things we can test:
+# initializers, intrinsic dimension, learning rate, network parameters
+
+epochs = 25
+for intrinsic_dim in range(5):
+    for initializer in ['glorot_normal', 'glorot_uniform']:
+        for lr in [0.1, 0.01, 0.001]:
+            for model_type in [mnist_classifier.build_linear_fc_projection_model,
+                               mnist_classifier.build_squared_fc_projection_model]:
+                model_type(intrinsic_dim=200+100*intrinsic_dim, lr=lr, initializer=initializer)
+                print(f'epochs: {epochs}; initializer: {initializer}; lr: {lr}; type: {"linear" if model_type == mnist_classifier.build_linear_fc_projection_model else "squared"}')
+                mnist_classifier.model.summary()
+                mnist_classifier.train(epochs=epochs)
+                mnist_classifier.evaluate()
--- models/mnist_classifier.py
+++ models/mnist_classifier.py
@@ -2,10 +2,11 @@
 Experimenting on MNIST
 """
 import tensorflow as tf
+from tensorflow.python.keras.regularizers import l2
 
 from keras_ext.intrinsic_weights import IntrinsicWeights
 from keras_ext.projection_layer import DenseRandomProjectionLayer
-from keras_ext.weight_creator import DenseLinearWeightCreator
+from keras_ext.weight_creator import DenseLinearWeightCreator, SquaredTermsWeightCreator
 
 
 class MNISTClassifier:
@@ -14,7 +15,7 @@ class MNISTClassifier:
         (self.x_train, self.y_train), (self.x_test, self.y_test) = tf.keras.datasets.mnist.load_data()
         self.model: tf.keras.models.Model = None
 
-    def build_fc_direct_model(self, layers=2, width=128):
+    def build_fc_direct_model(self, layers=2, width=128, lr=0.001, initializer='he_uniform'):
         """Simple fully-connected model with ordinary keras layers"""
         self.model = tf.keras.models.Sequential([
             tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
@@ -22,48 +23,81 @@ class MNISTClassifier:
             *[tf.keras.layers.Dense(width, activation='relu') for _ in range(layers)],
             tf.keras.layers.Dense(10)
         ])
+        self.model.compile(optimizer=tf.keras.optimizers.Adam(),
+                           loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                           metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
 
-    def build_fc_projection_model(self, layers=2, width=128, intrinsic_dim=200):
+    def build_linear_fc_projection_model(self, layers=2, width=128, intrinsic_dim=200,
+                                  lr=0.001, initializer='he_uniform'):
         """Fully-connected model with projection layers"""
         intrinsic_weights = IntrinsicWeights(size=intrinsic_dim)
-        weight_creator = DenseLinearWeightCreator(initial_weight_initializer='glorot_normal',
-                                                  projection_matrix_initializer='glorot_normal')
+        weight_creator = DenseLinearWeightCreator(initial_weight_initializer=initializer,
+                                                  projection_matrix_initializer=initializer)
+        # weight_creator = SquaredTermsWeightCreator(initial_weight_initializer='glorot_uniform',
+        #                                            projection_matrix_initializer='random_uniform')
 
         self.model = tf.keras.models.Sequential([
             tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
             tf.keras.layers.Flatten(),
             *[DenseRandomProjectionLayer(weight_creator, intrinsic_weights, width, activation='relu')
               for _ in range(layers)],
-            tf.keras.layers.Dense(10)
+            DenseRandomProjectionLayer(weight_creator, intrinsic_weights, 10)
         ])
+        self.model.compile(optimizer=tf.keras.optimizers.Adam(lr=lr),
+                           loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                           metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+
+    def build_squared_fc_projection_model(self, layers=2, width=128, intrinsic_dim=200,
+                                  lr=0.001, initializer='he_uniform'):
+        """Fully-connected model with projection layers"""
+        intrinsic_weights = IntrinsicWeights(size=intrinsic_dim)
+        # weight_creator = DenseLinearWeightCreator(initial_weight_initializer='glorot_normal',
+        #                                           projection_matrix_initializer='glorot_normal')
+        weight_creator = SquaredTermsWeightCreator(initial_weight_initializer=initializer,
+                                                   projection_matrix_initializer=initializer)
+
+        self.model = tf.keras.models.Sequential([
+            tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
+            tf.keras.layers.Flatten(),
+            *[DenseRandomProjectionLayer(weight_creator, intrinsic_weights, width, activation='relu')
+              for _ in range(layers)],
+            DenseRandomProjectionLayer(weight_creator, intrinsic_weights, 10)
+        ])
+        self.model.compile(optimizer=tf.keras.optimizers.Adam(lr=lr),
+                           loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                           metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
 
     def build_cnn_direct_model(self):
-        tf.keras.models.Sequential()
-        xx = Convolution2D(16, kernel_size=3, strides=1, init='he_normal', padding='valid', activation='relu', kernel_regularizer=l2(weight_decay))(preproc_images)
-        xx = Convolution2D(16, 3, 3, init='he_normal', padding='valid', activation='relu', kernel_regularizer=l2(weight_decay))(xx)
-        xx = MaxPooling2D((2, 2))(xx)
-        xx = Convolution2D(16, 3, 3, init='he_normal', padding='valid', activation='relu', kernel_regularizer=l2(weight_decay))(xx)
-        xx = BatchNormalization(momentum=0.5)(xx)
-        xx = Convolution2D(16, 3, 3, init='he_normal', padding='valid', activation='relu', kernel_regularizer=l2(weight_decay))(xx)  # (8, 8)
-        xx = MaxPooling2D((2, 2))(xx)  # (4, 4)
-        xx = Flatten()(xx)
-        xx = Dense(800, kernel_initializer='he_normal', activation='relu', kernel_regularizer=l2(weight_decay))(xx)
-        xx = Dense(800, kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay))(xx)
-        xx = BatchNormalization(momentum=0.5)(xx)
-        xx = Activation('relu')(xx)
-        xx = Dense(500, kernel_initializer='he_normal', activation='relu', kernel_regularizer=l2(weight_decay))(xx)
-        logits = Dense(10, kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay))(xx)
-        pass
+        weight_decay = 0
+
+        self.model = tf.keras.models.Sequential([
+            tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
+            tf.keras.layers.Conv2D(16, kernel_size=3, strides=1, init='he_normal', padding='valid', activation='relu', kernel_regularizer=l2(weight_decay)),
+            tf.keras.layers.Conv2D(16, 3, 3, init='he_normal', padding='valid', activation='relu', kernel_regularizer=l2(weight_decay)),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Conv2D(16, 3, 3, init='he_normal', padding='valid', activation='relu', kernel_regularizer=l2(weight_decay)),
+            tf.keras.layers.BatchNormalization(momentum=0.5),
+            tf.keras.layers.Conv2D(16, 3, 3, init='he_normal', padding='valid', activation='relu', kernel_regularizer=l2(weight_decay)),  # (8, 8)
+            tf.keras.layers.MaxPooling2D((2, 2)),  # (4, 4)
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(800, kernel_initializer='he_normal', activation='relu', kernel_regularizer=l2(weight_decay)),
+            tf.keras.layers.Dense(800, kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay)),
+            tf.keras.layers.BatchNormalization(momentum=0.5),
+            tf.keras.layers.Activation('relu'),
+            tf.keras.layers.Dense(500, kernel_initializer='he_normal', activation='relu', kernel_regularizer=l2(weight_decay)),
+            tf.keras.layers.Dense(10, kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay)),
+        ])
+        self.model.compile(optimizer=tf.keras.optimizers.Adam(),
+                           loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                           metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
 
     def build_cnn_projection_model(self):
         pass
 
     def train(self, epochs=100):
         assert self.model is not None
-        self.model.compile(optimizer=tf.keras.optimizers.Adam(),
-                           loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                           metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
-        self.model.fit(self.x_train, self.y_train, epochs=epochs)
+        self.model.fit(self.x_train, self.y_train, epochs=epochs,
+                       callbacks=[tf.keras.callbacks.LearningRateScheduler(lambda epoch, lr: lr * 0.999)])
 
     def evaluate(self):
         assert self.model is not None
