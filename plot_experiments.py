c4ccba0464e1c026c4ebfea6a360b2021c88b01d
rlee360 <Lee66@cooper.edu>
2020-12-15 23:41:15 -0500

    Plot script

--- plot_experiments.py
+++ plot_experiments.py
@@ -3,8 +3,14 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
+import matplotlib as mpl
+#set fonts
+font = {'family': 'cmr10', 'size': 18}
+mpl.rc('font', **font)  # change the default font to Computer Modern Roman
+mpl.rcParams['axes.unicode_minus'] = False  # because cmr10 does not have a Unicode minus sign
+
 # regular vs. power
-mnist_files = glob.glob('runs/mnist_nonnormalized_p/mnist*.pkl') + glob.glob('runs/rff_nonnormalized_p/*.pkl')
+mnist_files = glob.glob('runs/mnist_nonnormalized_p/mnist*.pkl')
 results_linear = []   # linear dense projection
 results_power = []   # power dense projection
 results_rff = []
@@ -19,19 +25,38 @@ for filename in mnist_files:
     else:
         results_rff.append([model_dict['intrinsic_dim'], model_dict['eval'][1]])
 
+mnist_files = glob.glob('runs/bad/mnist*.pkl')
+results_div = []
+for filename in mnist_files:
+    with open(filename, 'rb') as file_handle:
+        model_dict = pickle.load(file_handle)
+    results_div.append([model_dict['intrinsic_dim'], model_dict['eval'][1]])
+
 print(results_linear)
 print(results_power)
 
-#plt.figure()
-results = np.array(results_linear)
-plt.scatter(results[:, 0], results[:, 1])
-results = np.array(results_power)
-plt.scatter(results[:, 0], results[:, 1])
-results = np.array(results_rff)
-plt.scatter(results[:, 0], results[:, 1], color='k')
-plt.title('784-200-200 FC MNIST Accuracy vs. Intrinsic Dimension')
+plt.figure(figsize=(8,6))
+# results = np.array(sorted(results_linear))
+# print(results)
+# plt.plot(results[:, 0], results[:, 1], 'bo-', label='Linear')
+# results = np.array(sorted(results_power))
+# #results[8][1] = 0.8362
+# plt.plot(results[:, 0], results[:, 1], 'ro-', label='Power')
+results = np.array(sorted(results_rff))[4:]
+#results[8][1] = 0.8069
+plt.plot(results[:, 0], results[:, 1], 'ko-', label='RFF lr = 0.0001')
+results = np.array(sorted(results_rff)[:4] + sorted(results_div))
+plt.plot(results[:, 0], results[:, 1], 'o-', label='RFF lr = 0.001', color="#ca5a95")
+#plt.title('784-200-200 FC MNIST Accuracy vs. Intrinsic Dimension')
 plt.ylabel('Accuracy')
 plt.xlabel('Intrinsic dimension')
+plt.grid()
+#plt.yticks(np.arange(0,1,0.05))
+#plt.xticks(np.arange(0,1100,100))
+#plt.ylim(top=1)
+plt.xlim(0,1025)
+plt.legend(loc='lower left')
+plt.savefig('plots/mnist_nonnormalized_p_rff.pdf')
 
 # power w/ different coefficients
 # mnist_files = glob.glob('runs/power_coefficients/mnist*.pkl')
